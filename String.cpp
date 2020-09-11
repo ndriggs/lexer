ee5848f9ea0bda0bf82d46f409920b17deb52f72
Nathaniel Dirggs <nathanieldriggs@gmail.com>
2020-09-11 09:56:14 -0600

    Finished!

--- String.cpp
+++ String.cpp
@@ -6,12 +6,23 @@ String::String(){
 }
 
 int String::Read(const string& input){
+	newLines = 0;
+	bool state_even = false;
 	if(input[0] != '\''){
 		return 0;
 	}
 	for(int i = 1; (unsigned)i < input.size(); i++){
+		if(input[i] == '\n')
+			newLines += 1;	
+		if(state_even){
+			if(input[i] != '\''){
+				if(input[i] == '\n')
+					newLines -= 1;	
+				return i;
+			}
+		}
 		if(input[i] == '\''){
-			return i + 1;
+			state_even = !state_even;
 		}
 	}
 	return 0;
